bef55890f6d802a24c164d7b2b7d883b8b2421c4
wkoju84 <wkoju84@gmail.com>
2024-10-10 13:53:24 -0300

    first commit

---
+++ src/main/java/com/william/usuario/UsuarioApplication.java
@@ -0,0 +1,14 @@
+package com.william.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UsuarioApplication.class, args);
+	}
+
+}
